7c8180ae2e403a21172dba327c4c51a30df7187d
Anurag Roy <anuragr9847@gmail.com>
2021-05-21 16:57:01 +0530

    Add emoji font for colored emoji support

--- config.def.h
+++ config.def.h
@@ -10,7 +10,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "sans-serif:size=11" };
+static const char *fonts[]          = {
+	"sans-serif:size=11",
+	"Noto Color Emoji:size=11",
+};
 static const char dmenufont[]       = "monospace:size=11";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";

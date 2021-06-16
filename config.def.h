37c6eb6a33f3b4e20ad562a1afbfabb4a0728cc1
Anurag Roy <anuragr9847@gmail.com>
2021-06-16 17:16:10 +0530

    Apply decoration hints patch

--- config.def.h
+++ config.def.h
@@ -54,6 +54,7 @@ static const Rule rules[] = {
 static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
 static int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int decorhints  = 1;    /* 1 means respect decoration hints */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */

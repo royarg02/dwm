db8e636b8a824138678a8b547c8afd1aed4f0678
Anurag Roy <anuragr9847@gmail.com>
2021-06-17 20:03:18 +0530

    Apply swap focus patch

--- dwm.c
+++ dwm.c
@@ -258,6 +258,7 @@ static void showhide(Client *c);
 static void sigchld(int unused);
 static void sigstatusbar(const Arg *arg);
 static void spawn(const Arg *arg);
+static void swapfocus();
 static Monitor *systraytomon(Monitor *m);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
@@ -297,6 +298,7 @@ static void resource_load(XrmDatabase db, char *name, enum resource_type rtype, 
 
 /* variables */
 static Systray *systray =  NULL;
+static Client *prevclient = NULL;
 static const char broken[] = "broken";
 static char stext[256];
 static int statusw;
@@ -1977,6 +1979,17 @@ spawn(const Arg *arg)
 }
 
 void
+swapfocus()
+{
+	Client *c;
+	for(c = selmon->clients; c && c != prevclient; c = c->next) ;
+	if(c == prevclient) {
+		focus(prevclient);
+		restack(prevclient->mon);
+	}
+}
+
+void
 tag(const Arg *arg)
 {
 	if (selmon->sel && arg->ui & TAGMASK) {
@@ -2090,6 +2103,7 @@ unfocus(Client *c, int setfocus)
 {
 	if (!c)
 		return;
+	prevclient = c;
 	grabbuttons(c, 0);
 	XSetWindowBorder(dpy, c->win, scheme[SchemeNorm][ColBorder].pixel);
 	if (setfocus) {
@@ -2679,12 +2693,13 @@ void
 zoom(const Arg *arg)
 {
 	Client *c = selmon->sel;
+	prevclient = nexttiled(selmon->clients);
 
 	if (!selmon->lt[selmon->sellt]->arrange
 	|| (selmon->sel && selmon->sel->isfloating))
 		return;
 	if (c == nexttiled(selmon->clients))
-		if (!c || !(c = nexttiled(c->next)))
+		if (!c || !(c = prevclient = nexttiled(c->next)))
 			return;
 	pop(c);
 }
